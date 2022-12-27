cc90fc67b04c7c0a42b64dbf14b2950177f3265c
Arka Bhuiyan <arka.bhuiyan@bjitgroup.com>
2022-12-27 08:33:25 +0600

    initiated the whole project and implemented basic flux and mono

---
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -0,0 +1,28 @@
+package com.learnreactiveprogramming.service;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class FluxAndMonoGeneratorService {
+
+    public Flux<String> nameFlux() {
+        return Flux.fromIterable(List.of("Arka","Rabbi","Mosfik","Tahiyat","Zareen"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("Arka Bhuiyan");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.nameFlux().subscribe(name -> {
+            System.out.println("Name is : " + name + ".");
+        });
+
+        fluxAndMonoGeneratorService.nameMono().subscribe(name -> {
+            System.out.println("Mono Name is : " + name + ".");
+        });
+    }
+}
